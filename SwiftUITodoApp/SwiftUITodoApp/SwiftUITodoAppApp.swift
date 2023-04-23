32c3b809cced970f05ebae6845743ec429082eb9
山本朋哉 <>
2023-04-23 17:17:13 +0900

    プロジェクト作成

---
+++ SwiftUITodoApp/SwiftUITodoApp/SwiftUITodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoAppApp.swift
+//  SwiftUITodoApp
+//
+//  Created by 山本朋哉 on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
