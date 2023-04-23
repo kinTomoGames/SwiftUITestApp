b5b2d5a26e8f2d4af001f04060dc2af38a591457
山本朋哉 <>
2023-04-23 17:10:58 +0900

    カスタマイズトグルで勉強完了

--- ToDoTestApp/ToDoTestApp/ContentView.swift
+++ ToDoTestApp/ToDoTestApp/ContentView.swift
@@ -15,44 +15,27 @@ struct BattingOrder {
 
 struct ContentView: View {
     
-    @State private var battingOrderDataList: [BattingOrder] = [
-        BattingOrder(num: 1, pos: "センター", name: "イチロー"),
-        BattingOrder(num: 2, pos: "セカンド", name: "山田 哲人"),
-        BattingOrder(num: 3, pos: "ファースト", name: "小笠原 道大"),
-        BattingOrder(num: 4, pos: "DH", name: "大谷 翔平"),
-        BattingOrder(num: 5, pos: "サード", name: "村上 宗隆"),
-        BattingOrder(num: 6, pos: "センター", name: "松井 秀喜"),
-        BattingOrder(num: 7, pos: "ショート", name: "坂本 隼人"),
-        BattingOrder(num: 8, pos: "レフト", name: "吉田 正尚"),
-        BattingOrder(num: 9, pos: "キャッチャー", name: "古田 敦也")]
-    
-    @State private var textFieldTest: String = ""
-    
-    @ObservedObject var userData = UserData(name: "田中", age: 20)
-    
-    @State private var isShowingSheet = false
+    @State private var isToggle = false
     
     var body: some View {
-        Toggle("トグルのテキスト", isOn: $isShowingSheet)
-            .padding(10)
-            .disabled(isShowingSheet)
-        
-//        Button(action: {
-//            isShowingSheet = true
-//        }) {
-//            Text("テスト用のシート表示")
-//        }
-        .sheet(isPresented: $isShowingSheet, content: {
-            testSheet(isShowingSheet: $isShowingSheet)
-        })
+        Toggle("トグル作成", isOn: $isToggle)
+            .toggleStyle(TestToggleStyle())
     }
 }
 
-struct testSheet: View {
-    @Binding var isShowingSheet: Bool
-    
-    var body: some View {
-        Text(isShowingSheet.description)
+struct TestToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Button("イチロー") {
+                configuration.isOn.toggle()
+            }
+            .disabled(configuration.isOn)
+            
+            Button("小笠原") {
+                configuration.isOn.toggle()
+            }
+            .disabled(!configuration.isOn)
+        }
     }
 }
 
